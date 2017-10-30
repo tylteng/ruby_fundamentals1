d7ab66f5e3362e40f9be3a2ae96789f622ba2618
tylteng <teng.ty4@gmail.com>
2017-10-30 15:21:03 -0400

    finished exercise2

---
+++ exercise2.rb
@@ -0,0 +1,21 @@
+#Question 1
+#Well firstly I would have to dictate what a proper tip should be.
+#For the sake of the exercise I will say 15% so now I will work to make this calculation.
+#So now I will tell the code to divide the total amount by 100 and then multiply that number by 15.
+
+puts ( 55.0 / 100.0 ) * 15
+
+#Question 2
+#In order to solve this problem, the integer will have to be placed into a string.
+
+puts "Hello" + "1"
+
+#Question 3
+#I will be placing the integers inside #{} which will then be included inside ""
+
+puts "Hello class, #{45628*7839}!!"
+
+#Question 4
+#The boolean value of this expression is True.
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
